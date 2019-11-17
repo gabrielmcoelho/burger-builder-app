c0fd3238b2d33f76f69f5cddf4cae688f38f0fa4
Gabriel Coelho <gabriel_mcoelho@hotmail.com>
2019-11-17 16:14:39 -0300

    map list of build controls into template

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl'
+
+const controls = [
+    {label: 'Meat', type: 'meat'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Salad', type: 'salad'}
+];
 
 const buildControls = (props) => {
     return(
         <div className={classes.BuildControls}>
-
+            {controls.map(control => <BuildControl key={control.label} label={control.label}/>)}
         </div>
     )
 };
