fa3a513d93a5e099687b23bf79658955036787ed
Gabriel Coelho <gabriel_mcoelho@hotmail.com>
2019-11-17 17:42:37 -0300

    add modal component to show order summary

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux'
+
+const orderSummary = (props) => {
+
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(ingredient => {
+            return (
+                <li key={ingredient}>
+                    <span style={{textTransform: 'capitalize'}}>{ingredient}</span>
+                    : {props.ingredients[ingredient]}
+                </li>
+            )
+        });
+
+    return (
+        <Aux>
+            <h3>Your order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to checkout?</p>
+        </Aux>
+    );
+};
+
+export default orderSummary;
