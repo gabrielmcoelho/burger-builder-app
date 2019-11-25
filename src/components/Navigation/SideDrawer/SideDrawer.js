e54ef037a1512ee18ff8ad727d99a0f76b4c5c03
Gabriel Coelho <gabriel_mcoelho@hotmail.com>
2019-11-25 19:10:05 -0300

    move Layout component to hoc directory

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux/Aux'
 import classes from './SideDrawer.css'
 
 const sideDrawer = (props) => {
