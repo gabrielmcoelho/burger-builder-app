1186962e6efad9279bfd2522d9d4b9379b2c103b
Gabriel Coelho <gabriel_mcoelho@hotmail.com>
2019-11-25 19:02:31 -0300

    change modal component to class based component | add shouldComponentUpdate to modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from "prop-types";
 
 import classes from './Modal.css'
 import Aux from '../../../hoc/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} hideModal={props.hideModal}/>
-        <div className={classes.Modal}
-             style={{
-                 transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                 opacity: props.show ? '1': '0'
-             }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
 
-modal.propTypes = {
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return nextProps.show !== this.props.show;
+    }
+
+    render() {
+        return(
+            <Aux>
+                <Backdrop show={this.props.show} hideModal={this.props.hideModal}/>
+                <div className={classes.Modal}
+                     style={{
+                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                         opacity: this.props.show ? '1': '0'
+                     }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    }
+
+}
+
+Modal.propTypes = {
     show: PropTypes.bool,
     hideModal: PropTypes.func
 };
 
-export default modal;
+export default Modal;
