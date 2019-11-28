0a1d54ea9163120f3eaada700debd3b7d4e17b85
Gabriel Coelho <gabriel_mcoelho@hotmail.com>
2019-11-28 00:24:34 -0300

    add axios dependency | send post request to firebase after purchase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Aux from '../../hoc/Aux/Aux'
 import Burger from '../../components/Burger/Burger'
@@ -71,7 +72,28 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        alert("thanks for purchasing!");
+        // alert("thanks for purchasing!");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Coelho',
+                address: {
+                    street: 'Ibituruna',
+                    zipCode: '1234',
+                    country: 'Brazil'
+                },
+                email: 'dummy@dummy.com'
+            },
+            deliveryMethod: 'cheapest'
+        };
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     };
 
     render() {
